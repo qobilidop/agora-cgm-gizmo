a30d3b8d04da35a315ea70af5782888fd2496f59
qobilidop <qobilidop@gmail.com>
2018-07-23 06:07:53 -0700

    Reorganize task directory

--- code/task/1-isolated/1-init-all.sh
+++ code/task/1-isolated/1-init-all.sh
@@ -2,6 +2,6 @@
 set -e
 
 cd "$(dirname "$0")"
-for init_script in init-sim-*.sh; do
+for init_script in script/init-sim-*.sh; do
     ./"$init_script"
 done
--- code/task/1-isolated/2-submit-all-on-tscc.sh
+++ code/task/1-isolated/2-submit-all-on-tscc.sh
@@ -2,6 +2,6 @@
 set -e
 
 cd "$(dirname "$0")"
-for job in sim/*/job-tscc.sh; do
+for job in script/sim/*/job-tscc.sh; do
     qsub "$job"
 done
