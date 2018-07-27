f3844dff9df46d1250ada924fba5114634039383
qobilidop <qobilidop@gmail.com>
2018-07-26 21:04:33 -0700

    Install MUSIC in conda env

---
+++ code/extern/music/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p "$LOCAL_PREFIX/opt"
+cd "$LOCAL_PREFIX/opt"
+
+# Download
+remote=https://bitbucket.org/ohahn/music
+branch=default
+hg clone "$remote" -u "$branch"
+cd music
+
+# Patch
+patch < "$PROJECT_DIR/code/extern/music/Makefile.diff"
+
+# Compile
+export CC="$LOCAL_CXX"
+export OPT="-Wno-unknown-pragmas"
+export CFLAGS="$LOCAL_CFLAGS"
+export LFLAGS="$LOCAL_LDFLAGS -lgsl -lgslcblas"
+export CPATHS="-I. "$LOCAL_FFTW3_INC" "$LOCAL_GSL_INC" "$LOCAL_HDF5_INC""
+export LPATHS=""$LOCAL_FFTW3_LIB" "$LOCAL_GSL_LIB" "$LOCAL_HDF5_LIB""
+make -j
+
+# Install
+mv MUSIC "$LOCAL_PREFIX/bin/"
+
+# Test
+which MUSIC
---
+++ code/extern/music/uninstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd "$LOCAL_PREFIX"
+rm -rf bin/MUSIC opt/music
