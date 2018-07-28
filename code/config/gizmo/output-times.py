b875ba5c0f5eba6ffabd7f6d8176f17acd0f2701
qobilidop <qobilidop@gmail.com>
2018-07-28 12:24:39 -0700

    Simplify script

--- code/config/gizmo/output-times.py
+++ code/config/gizmo/output-times.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import numpy as np
 
-a_uniform = np.linspace(0, 1, 201)
+a = np.linspace(0, 1, 201)
 z = np.array([6, 3, 2, 1, 0.5, 0.2, 0.0])
 z = np.concatenate([z, z + 0.05, z - 0.05])
-a_special = 1 / (1 + z)
-a_final = np.sort(np.unique(np.concatenate([a_uniform, a_special])))
-np.savetxt('output-times.txt', a_final)
+a = np.concatenate([a, 1 / (1 + z)])
+a = np.sort(np.unique(a))
+np.savetxt('output-times.txt', a)
