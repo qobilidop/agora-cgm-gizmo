deb3dccf35072c9f8d9f74cc2644230285abcd17
qobilidop <qobilidop@gmail.com>
2018-07-29 15:39:56 -0700

    Make cosmological IC in conda
    It's taking me too much time to figure out how to compile MUSIC properly on Edison.

--- code/task/2-cosmological/1-conda-make-ic.sh
+++ code/task/2-cosmological/1-conda-make-ic.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-#PBS -N make-ic
-#PBS -q condo
-#PBS -l nodes=1:ppn=16
-#PBS -l walltime=1:00:00
-#PBS -V
-#PBS -j oe
-#PBS -o tscc-job-make-ic.log
 set -e
 
 cd "$REPO_DIR"
--- code/task/2-cosmological/1-tscc-make-ic.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")"
-cd script
-qsub tscc-job-make-ic.sh
