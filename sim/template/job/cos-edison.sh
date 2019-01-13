b9ca5c836591eff8c7504e1aa5eee0ac1cf9679b
qobilidop <qobilidop@gmail.com>
2019-01-13 14:09:23 -0800

    Refactor simulation template

--- sim/template/job/cos-edison.sh
+++ sim/template/job/cos-edison.sh
@@ -2,22 +2,25 @@
 #SBATCH -q regular
 #SBATCH -N 8
 #SBATCH -t 48:00:00
-#SBATCH -o edison.log
+#SBATCH -o run.log
 #SBATCH -L SCRATCH
 #SBATCH --export=ALL
 #SBATCH -D .
 set -e
 cd ..
 echo "$PWD"
+source gizenv-activate.sh
 
-date
+# See https://docs.nersc.gov/jobs/examples/#hybrid-mpiopenmp-jobs
 export OMP_PROC_BIND=true
 export OMP_PLACES=threads
 export OMP_NUM_THREADS=2
+
+date
 ncpus=$(($OMP_NUM_THREADS*2))
 ntasks=$(($SLURM_CPUS_ON_NODE/$ncpus))
 opts="--ntasks-per-node $ntasks --cpus-per-task $ncpus"
-run_gizmo="srun $opts ./GIZMO params.txt"
+run_gizmo="srun $opts ./GIZMO gizmo-params.txt"
 if [ -d output/restartfiles ]; then
     $run_gizmo 1
 else
--- sim/template/job/iso-tscc.sh
+++ sim/template/job/iso-tscc.sh
@@ -3,18 +3,20 @@
 #PBS -l nodes=1:ppn=16
 #PBS -l walltime=8:00:00
 #PBS -j oe
-#PBS -o tscc.log
+#PBS -o run.log
 #PBS -V
 #PBS -d .
 set -e
 cd ..
 echo "$PWD"
+source gizenv-activate.sh
+
+export OMP_NUM_THREADS=1
 
 date
-export OMP_NUM_THREADS=1
 ntasks=$((16/$OMP_NUM_THREADS))
 opts="-v -machinefile $PBS_NODEFILE -npernode $ntasks -x LD_LIBRARY_PATH"
-run_gizmo="mpirun $opts ./GIZMO params.txt"
+run_gizmo="mpirun $opts ./GIZMO gizmo-params.txt"
 if [ -d output/restartfiles ]; then
     $run_gizmo 1
 else
