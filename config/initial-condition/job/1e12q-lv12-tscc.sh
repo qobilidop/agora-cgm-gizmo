9613e7acd91863c3e2a5a17b290d91658e2b5765
qobilidop <qobilidop@gmail.com>
2019-01-13 18:38:48 -0800

    Update IC job

--- config/initial-condition/job/1e12q-lv12-tscc.sh
+++ config/initial-condition/job/1e12q-lv12-tscc.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-#PBS -N ic-1e12q
+#PBS -N 1e12q-lv12
 #PBS -q condo
 #PBS -l nodes=1:ppn=16
 #PBS -l walltime=8:00:00
 #PBS -j oe
-#PBS -o tscc-ic-1e12q.log
+#PBS -o 1e12q-lv12-tscc.log
 #PBS -V
 #PBS -d .
 set -e
 cd ..
 echo "$PWD"
+source gizenv-activate.sh
 
 date
-MUSIC 1e12q.conf
+MUSIC 1e12q-lv12.conf
 date
